d935923708b6077a553bd0253eec2b4b1045a958
Jakub <jakub.bchyla@gmail.com>
2023-01-08 10:54:47 +0100

    first commit

---
+++ backend/english-sentences/src/main/java/com/jakubchyla/englishsentences/EnglishSentencesApplication.java
@@ -0,0 +1,13 @@
+package com.jakubchyla.englishsentences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishSentencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishSentencesApplication.class, args);
+	}
+
+}
---
+++ backend/english-sentences/src/test/java/com/jakubchyla/englishsentences/EnglishSentencesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakubchyla.englishsentences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishSentencesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
