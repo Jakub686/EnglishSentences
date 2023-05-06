16ae4c65865f96f7610dfd82afbe5bb493ddef06
Jakub <jakub.bchyla@gmail.com>
2023-05-06 10:15:00 +0200

    configure databese

--- backend/english-sentences/src/main/java/com/jakubchyla/englishsentences/token/Token.java
+++ backend/english-sentences/src/main/java/com/jakubchyla/englishsentences/token/Token.java
@@ -1,11 +1,12 @@
 package com.jakubchyla.englishsentences.token;
 
+import com.jakubchyla.englishsentences.user.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.userdetails.User;
+
 
 @Data
 @Builder
