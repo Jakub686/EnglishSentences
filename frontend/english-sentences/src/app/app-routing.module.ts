1b9d1f9fab7023b4fabf17e1119b4221bd406308
Jakub <jakub.bchyla@gmail.com>
2023-05-14 15:42:46 +0200

    add login component

--- frontend/english-sentences/src/app/app-routing.module.ts
+++ frontend/english-sentences/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {MainComponent} from "./main/main.component";
 import {CreateSentenceComponent} from "./create-sentence/create-sentence.component";
 import {UpdateSentenceComponent} from "./update-sentence/update-sentence.component";
 import {SentenceDetailsComponent} from "./sentence-details/sentence-details.component";
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'main', pathMatch: 'full'}, // redirect from '' to main
@@ -13,6 +14,7 @@ const routes: Routes = [
   {path: 'main', component: MainComponent},
   {path: 'update-sentence/:id', component: UpdateSentenceComponent},
   {path: 'details-sentence/:id', component: SentenceDetailsComponent},
+  {path: 'login', component: LoginComponent},
   {path: '**', redirectTo: 'main'}, // redirect from '**' to main
 
 ];
