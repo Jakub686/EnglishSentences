4d74dbafa9d46bba673b63b0b1d945643804e347
Jakub <jakub.bchyla@gmail.com>
2023-06-18 20:23:49 +0200

    CORS problem with secured endpoint on FE

--- frontend/english-sentences/src/app/create-sentence/create-sentence.component.ts
+++ frontend/english-sentences/src/app/create-sentence/create-sentence.component.ts
@@ -3,33 +3,48 @@ import {Sentence} from "../model/sentence";
 import {Router} from "@angular/router";
 import {SentenceService} from "../service/sentence.service";
 import {TranslationToPl} from "../model/translationToPl";
+import {UserService} from "../service/user.service";
 
 @Component({
   selector: 'app-create-sentence',
   templateUrl: './create-sentence.component.html',
   styleUrls: ['./create-sentence.component.css']
 })
-export class CreateSentenceComponent{
+export class CreateSentenceComponent {
 
-  sentence: Sentence = {id: 0, textEn:"",translationToPl:[{id:0,textPl:""},{id:1,textPl:""}]};
-  constructor(private sentenceService: SentenceService, private router: Router) {
+  token: string;
+  sentence: Sentence = {id: 0, textEn: "", translationToPl: [{id: 0, textPl: ""}, {id: 1, textPl: ""}]};
+
+  constructor(private sentenceService: SentenceService, private userService: UserService, private router: Router) {
+    this.token = localStorage.getItem('token') as string;
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.sentence);
     console.log(this.sentence.textEn);
     console.log(this.sentence.translationToPl[0].textPl);
     this.saveSentence();
     this.goToSentenceList();
   }
-  saveSentence(){
-    this.sentenceService.createSentenceSimple(this.sentence).subscribe(data =>{
-    console.log(data);
-    },
+
+  saveSentence() {
+    this.sentenceService.createSentenceSimple(this.sentence).subscribe(data => {
+        console.log(data);
+      },
       error => console.log(error));
   }
 
-  goToSentenceList(){
+  getContent() {
+    console.log('token i send ' + this.token)
+    this.userService.getSecureContent(this.token).subscribe(
+      data => {
+        console.log(data); // The response is a string, so you can directly access it
+      },
+      error => console.log(error)
+    );
+  }
+
+  goToSentenceList() {
     this.router.navigate(['/sentences'])
   }
 }
