f44e36faebbd0ba53171bd9aebba37e6094555e8
Jakub <jakub.bchyla@gmail.com>
2023-09-30 10:45:03 +0200

    added findSentenceListForUser test

--- backend/english-sentences/src/test/java/com/jakubchyla/englishsentences/SentenceServiceTest.java
+++ backend/english-sentences/src/test/java/com/jakubchyla/englishsentences/SentenceServiceTest.java
@@ -1,14 +1,18 @@
 package com.jakubchyla.englishsentences;
 
 import com.jakubchyla.englishsentences.model.Sentence;
+import com.jakubchyla.englishsentences.sentenceController.dto.RandomDTO;
 import com.jakubchyla.englishsentences.sentenceRepository.SentenceRepository;
 import com.jakubchyla.englishsentences.sentenceService.SentenceService;
+import com.jakubchyla.englishsentences.user.User;
+import com.jakubchyla.englishsentences.user.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -26,6 +30,42 @@ class SentenceServiceTest {
     @MockBean
     private SentenceRepository sentenceRepository;
 
+    @MockBean
+    private UserRepository userRepository;
+
+    @Test
+    public void test_findSentenceListForEmail_whenValid_returnsRandomDTOList() {
+
+        //given
+        Sentence sentence1 = new Sentence();
+        sentence1.setId(1L);
+        sentence1.setTextEn("Text1");
+        sentence1.setTextPl("Text1");
+
+        Sentence sentence2 = new Sentence();
+        sentence2.setId(2L);
+        sentence2.setTextEn("Text2");
+        sentence2.setTextPl("Text2");
+        List<Sentence> sentences = new ArrayList<>();
+        sentences.add(sentence1);
+        sentences.add(sentence2);
+
+        User user = new User();
+        user.setId(1L);
+        user.setFirstname("Jakub");
+        user.setLastname("Chyla");
+        user.setEmail("jakub@gmail.com");
+
+        //when
+        when(sentenceRepository.findAll()).thenReturn(sentences);
+        when(userRepository.findByEmail("jakub@gmail.com")).thenReturn(Optional.of(user));
+        List<RandomDTO> result = sentenceService.findSentenceListForEmail("jakub@gmail.com");
+
+        //then
+        assertEquals("Text2", result.get(1).textEn());
+        assertEquals(2, result.size());
+
+    }
 
     @Test
     public void test_SentenceService_FindByText_WithValidText_ReturnsListWithOneSentence() {
@@ -46,7 +86,7 @@ class SentenceServiceTest {
     }
 
     @Test
-    public void testGetRandomSentence_whenValidIdsAreProvided_returnsRandomSentence() {
+    public void test_SentenceService_GetRandomSentence_whenValidIdsAreProvided_returnsRandomSentence() {
 
         //given
         Long optionalLong1 = 1L;
@@ -59,12 +99,12 @@ class SentenceServiceTest {
         sentence.setTextEn("Text1");
         sentence.setTextPl("Text1");
 
-        //then
+        //when
         when(sentenceRepository.findAllIds()).thenReturn(optionalIds);
         when(sentenceRepository.findById(1L)).thenReturn(Optional.of(sentence));
         Sentence result = sentenceService.getRandomSentence();
 
-        //when
+        //then
         assertEquals("Text1", result.getTextEn());
     }
 
